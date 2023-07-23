3adf3208f253cc2687b04df8d4f655bfe4090663
twan <thisiscasillas@gmail.com>
2023-07-22 20:57:19 -0700

    New api route for all auth all playlists and its dependency

--- backend/app/api/v1/dependencies.py
+++ backend/app/api/v1/dependencies.py
@@ -1,8 +1,27 @@
 import os
+import redis
 from .services.spotify import SpotifyService
+from fastapi import HTTPException, status, Depends
+
+
+def get_redis():
+    return redis.Redis(host='localhost', port=6379, db=0)
 
 
 def get_spotify_service():
     client_id = os.getenv('CLIENT_ID')
     client_secret = os.getenv('CLIENT_SECRET')
     return SpotifyService(client_id, client_secret)
+
+
+def get_user_spotify_service(redis=Depends(get_redis)):
+    access_token = redis.get('access_token')
+    refresh_token = redis.get('refresh_token')
+    if access_token is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Not authenticated',
+        )
+    client_id = os.getenv('CLIENT_ID')
+    client_secret = os.getenv('CLIENT_SECRET')
+    return SpotifyService(client_id, client_secret, access_token, refresh_token)
--- backend/app/api/v1/routes/playlists.py
+++ backend/app/api/v1/routes/playlists.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from ..dependencies import get_spotify_service
+from ..dependencies import get_spotify_service, get_user_spotify_service
 from ..services.spotify import SpotifyService
 
 
@@ -7,6 +7,12 @@ router = APIRouter()
 
 
 @router.get('/{playlist_id}')
-def read_playlist(playlist_id: str, spotify_service: SpotifyService = Depends(get_spotify_service)):
+def get_playlist(playlist_id: str, spotify_service: SpotifyService = Depends(get_spotify_service)):
     playlist = spotify_service.get_playlist(playlist_id)
     return playlist
+
+
+@router.get('/all_playlists')
+def get_all_protected_playlists(spotify_service: SpotifyService = Depends(get_user_spotify_service)):
+    playlists = spotify_service.get_protected_playlists()
+    return playlists
