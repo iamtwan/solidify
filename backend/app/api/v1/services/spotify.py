dd8f8d81f482707ce78f2858ae15107944b46f6c
twan <thisiscasillas@gmail.com>
2023-07-22 20:52:32 -0700

    Added new class methods for spotify 'Authenticate Code' flow

--- backend/app/api/v1/services/spotify.py
+++ backend/app/api/v1/services/spotify.py
@@ -4,16 +4,17 @@ import time
 
 
 class SpotifyService:
-    def __init__(self, client_id, client_secret):
+    def __init__(self, client_id, client_secret, access_token=None, refresh_token=None):
         self.base_url = 'https://api.spotify.com/v1'
         self.token_url = 'https://accounts.spotify.com/api/token'
         self.client_id = client_id
         self.client_secret = client_secret
-        self.token = None
+        self.token = access_token
+        self.refresh_token = refresh_token
         self.token_expires = 0
-        self.authenticate()
+        if not access_token:
+            self.authenticate()
 
-    # Client Credentials OAuth2 Flow
     def authenticate(self):
         auth_string = base64.b64encode(f'{self.client_id}:{self.client_secret}'.encode('utf-8')).decode('utf-8')
         headers = {'Authorization': f'Basic {auth_string}'}
@@ -27,17 +28,41 @@ class SpotifyService:
         self.token_expires = time.time() + token_data['expires_in']
 
     def check_access_token(self):
-        # expiry epoch time
-        print(self.token_expires)
-        if time.time() > self.token_expires:
-            self.authenticate()
+        if not self.token or time.time() > self.token_expires:
+            self.refresh_access_token()
+
+    def refresh_access_token(self):
+        if not self.refresh_token:
+            return
+
+        auth_string = base64.b64encode(f'{self.client_id}:{self.client_secret}'.encode('utf-8')).decode('utf-8')
+        headers = {'Authorization': f'Basic {auth_string}'}
+        data = {'grant_type': 'refresh_token', 'refresh_token': self.token}
+
+        response = requests.post(self.token_url, headers=headers, data=data)
+        response.raise_for_status()
+
+        token_data = response.json()
+        self.token = token_data['access_token']
+        self.token_expires = time.time() + token_data['expires_in']
 
     def get_playlist(self, playlist_id):
-        print('test')
         self.check_access_token()
 
         fields = 'name,tracks.total,tracks.items(track(name,artists(name),album(name)))'
         url = f'{self.base_url}/playlists/{playlist_id}?fields={fields}'
+        headers = {'Authorization': f'Bearer {self.token}'}
+
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        return response.json()
+
+    def get_protected_playlists(self):
+        self.check_access_token()
+
+        fields = 'items(id,name,public)'
+        url = f'{self.base_url}/me/playlists?fields={fields}'
         headers = {'Authorization': f'Bearer {self.token}'}
 
         response = requests.get(url, headers=headers)
